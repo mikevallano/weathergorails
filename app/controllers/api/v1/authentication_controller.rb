a5f1f703f1c1192e94b79ae1b63a604d76b53f62
mike vallano <mikevallano@gmail.com>
2019-04-08 10:31:15 -0500

    Use JWT with devise. Pass JWT to warden to handle sessions

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::AuthenticationController < Api::ApiController
   skip_before_action :verify_authenticity_token # not needed because it's an API, not a form
-  skip_before_action :authenticate_token! # not needed because there is no token yet
+  # skip_before_action :authenticate_token! # not needed because there is no token yet
 
   def create
     user = User.find_by(email: params[:user][:email])
